365023cbb087ac9fdedd1368713d07338635f23e
Marek <marek.mikulski.14@gmail.com>
2018-11-01 21:58:45 +0100

    now Bank is a double connected list
    still to do:
    - transfers
    - save()
    - load()

---
+++ Bank/Bank/Main.cpp
@@ -0,0 +1,380 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+class BankAccount		// ---------------------------------------------- klasa BankAccount ---------------------------------------------------
+{
+public:
+	std::string firstName;	// imie wlascicela konta
+	std::string lastName;	// nazwisko wlasciciela konta
+	int accountNumber;		// numer konta ( 4 cyfrowy )
+	int balance;			// stan konta $$$
+
+	BankAccount* next;		// wskaznik na nastepne konto w banku
+	BankAccount* previous;
+
+
+	BankAccount();
+							//BankAccount(std::string first_name, std::string last_name, int account_number, int balance);
+	void withdraw();	// Wyp³aca / usuwa  pieniadze z konta
+	void deposit();		// wp³aca pieniadze na konto (dodaje pieniadze)
+	void showAccount();	// wyswietla aktualny stan konta
+};
+
+BankAccount::BankAccount() {
+	
+	balance = 0;
+	next = 0;
+	previous = 0;
+}
+
+									/*BankAccount::BankAccount(std::string first_name, std::string last_name, int account_number, int _balance) {
+										firstName = first_name;
+										lastName = last_name;
+										accountNumber = account_number;
+										balance = _balance;
+									}*/	
+
+void BankAccount::withdraw() {
+	std::cout << "\nHow much do you want to withdraw? ";
+	int cash;
+	do {
+		std::cin >> cash;
+		if (cash < 0)
+			std::cout << "\n\t You can't withdraw less than 0$\n";
+		else if (cash > balance)
+			std::cout << "\n\t You can't withdraw more than you have ("<<balance<<"$)\nTry again: ";
+	} while (cash<0 || cash>balance);
+	
+	balance -= cash;
+}
+
+void BankAccount::deposit() {
+	std::cout << "\nHow much do you want to deposit? ";
+	int cash;
+	do {
+		std::cin >> cash;
+		if (cash < 0)
+			std::cout << "\n\t You can't deposit less than 0$\n Try again: ";
+	} while (cash<0);
+
+	balance += cash;
+}
+
+void BankAccount::showAccount() {
+	std::cout << "\n\tFirst name: " << firstName ;
+	std::cout << "\n\tLast name: " << lastName;
+	std::cout << "\n\taccount number: " << accountNumber;
+	std::cout << "\n\taccount balance: " << balance << "$\n";
+}
+
+class Bank		// ---------------------------------------------------- klasa Bank --------------------------------------------------------------------------
+{
+public:
+
+	BankAccount* first;				// wskaznik na pierwsze konto w liscie ktora jest bank ( dzia³a na zasadzie listy dwukierunkowej )
+	BankAccount* last;
+
+	Bank();
+	void newAccount();				// zaklada nowe konto
+	void newAccount(std::string first_name, std::string last_name, int account_number, int _balance);	// zaklada konto dla podanych danych ( uzywam tylko przy load())
+	void deleteAccount();			// usuwa wskazane konto
+	void transfer();				// przelej pieniadze z jednego konta na drugie
+	void displayAllAccounts();		// wyswietl wszystkie konta
+	int howManyAccounts();			// zwraca iloœæ kont w banku
+	void save();					// zapisuje Bank w pliku tekstowym bank_data.txt
+	void load();					// wczytuje dane banku z pliku tekstowym bank_data.txt
+	void overviewMode();
+};
+
+Bank::Bank() {
+	first = 0;
+	last = 0;
+}
+
+void Bank::newAccount() {
+	BankAccount* nowy = new BankAccount;
+
+	std::cout << "\n\tCreating new account:\nfirst name: ";
+	std::cin >> nowy->firstName;
+	std::cout << "last name: ";
+	std::cin >> nowy->lastName;
+	std::cout << "account number: ";
+	std::cin >> nowy->accountNumber;
+
+	if (first == 0) {
+		first = nowy;
+		last = nowy;
+	}
+	else {
+		first->next = nowy;
+		nowy->previous = first;
+		nowy->next = 0;
+		first = nowy;
+	}
+}
+
+void Bank::newAccount(std::string firstName, std::string lastName, int accountNumber, int balance) {
+	BankAccount* nowy = new BankAccount;
+
+	nowy->firstName = firstName;
+	nowy->lastName = lastName;
+	nowy->accountNumber = accountNumber;
+	nowy->balance = balance;
+
+	if (first == 0) {
+		first = nowy;
+		last = nowy;
+	}
+	else {
+		first->next = nowy;
+		nowy->previous = first;
+		nowy->next = 0;
+		first = nowy;
+	}
+}
+
+void Bank::deleteAccount() {
+	BankAccount* temporary = last;
+	char decision;
+	std::cout << "Would you like to select account by last name or number? [L/N]";
+	std::cin >> decision;
+	do {
+		if (decision == 'l' || decision == 'L') {
+			std::string lastName;
+
+			std::cout << "Delete account of (type in the last name): ";
+			std::cin >> lastName;
+			while (lastName != temporary->lastName) {
+				temporary = temporary->next;
+			}
+			temporary->showAccount();
+			do {
+				std::cout << "\nDelete this account? [Y/N]: ";
+				std::cin >> decision;
+				if (decision != 'y' && decision != 'n')
+					std::cout << "You are supposed to choose Y (yes) or N (no). try again\n";
+			} while (decision != 'y' && decision != 'n');
+			if (decision == 'y') {
+				if (first == last) {
+					first = 0;
+					last = 0;
+				}
+				else if (temporary == first) {
+					first = first->previous;
+					first->next = 0;
+				}
+				else if (temporary == last) {
+					last = last->next;
+					last->previous = 0;
+				}
+				else {
+					temporary->previous->next = temporary->next;
+					temporary->next->previous = temporary->previous;
+				}
+				system("pause");
+			}
+			else {
+				std::cout << "\n\nYou decided not to delete this account\n\n ";
+				system("pause");
+			}
+		}
+		else if (decision == 'n' || decision == 'N') {
+			int number;
+			std::cout << "Delete account number (type in the account number): ";
+			std::cin >> number;
+			while (number != temporary->accountNumber) {
+				temporary = temporary->next;
+			}
+			temporary->showAccount();
+			do {
+				std::cout << "\nDelete this account? [Y/N]: ";
+				std::cin >> decision;
+				if (decision != 'y' && decision != 'n')
+					std::cout << "You are supposed to choose Y (yes) or N (no). try again\n";
+			} while (decision != 'y' && decision != 'n');
+			if (decision == 'y') {
+				if (first == last) {
+					first = 0;
+					last = 0;
+				}
+				else if (temporary == first) {
+					first = first->previous;
+					first->next = 0;
+				}
+				else if (temporary == last) {
+					last = last->next;
+					last->previous = 0;
+				}
+				else {
+					temporary->previous->next = temporary->next;
+					temporary->next->previous = temporary->previous;
+				}
+				system("pause");
+			}
+			else {
+				std::cout << "\n\nYou decided not to delete this account\n\n ";
+				system("pause");
+			}
+		}
+		else
+			std::cout << "You are supposed to type L (last name) or N (number) letter. Try again\n";
+	} while (decision != 'l' && decision != 'n' && decision == 'N' && decision == 'L');
+
+	//delete temporary;									<------ dopóki nie usun¹³em tego by³y problemy z displayAllAccounts() gdy wywo³ywa³em to po deleteAccount()
+}
+
+void Bank::transfer() {
+
+}
+
+void Bank::displayAllAccounts() {
+	BankAccount* temporary = last;
+	int counter = 1;
+	while (temporary) {								// znaczy to samo co while(temporary!=0)
+		std::cout << counter << ".";
+		temporary->showAccount();
+		temporary = temporary->next;
+		counter++;
+	}
+	std::cout << "\n\nfinished the displayAllAccounts() function\n\n";
+	delete temporary;
+}
+
+int Bank::howManyAccounts() {
+	BankAccount* temporary = last;
+	int counter = 0;
+	for (temporary = last; temporary; temporary = temporary->next)
+		counter++;
+
+	return counter;
+}
+
+void Bank::save() {
+
+}
+
+void Bank::load() {
+
+}
+
+void Bank::overviewMode() {
+	BankAccount* temporary = last;
+	if (last == 0) {
+		std::cout << "unable to open the overviewMode. There are no accounts...\n";
+	}
+	else {
+		int wybor;
+		do {
+			system("cls");
+			temporary->showAccount();
+			std::cout << "\n1.Next | 2.Previous | 3.Deposit | 4.Withdraw | 0.Exit overviewMode\n\tchoose option: ";
+			std::cin >> wybor;
+			if (wybor == 1) {
+				if (temporary->next == 0) {
+					std::cout << "\n\tCould't find next account.\n";
+					system("pause");
+				}
+				else
+					temporary = temporary->next;
+			}
+			else if (wybor == 2)
+				if (temporary->previous == 0) {
+					std::cout << "\n\tCould't find previous account.\n";
+					system("pause");
+				}
+				else
+					temporary = temporary->previous;
+			else if (wybor == 3)
+				temporary->deposit();
+			else if (wybor == 4)
+				temporary->withdraw();
+			else if (wybor == 0) {
+			}
+			else {
+				std::cout << "\n\tThere is no such an option. Try Again...";
+				system("pause");
+			}
+		} while (wybor != 0);
+	}
+}
+
+int main()		// -------------------------------------------- G³ówna funkcja -----------------------------------------------
+{
+	Bank* myBank = new Bank;
+
+	int wybor;
+	do {
+		system("cls");
+		std::cout << "   ---------------------------------------------   " << std::endl;
+		std::cout << "   ----=======>> The BANK  <<=======---  \n\n";
+		std::cout << "   1. Create new bank account  " << std::endl;
+		std::cout << "   2. Delete bank account  " << std::endl;
+		std::cout << "   3. Transfer money " << std::endl;
+		std::cout << "   4. Display all accounts  " << std::endl;
+		std::cout << "   5. How many accounts are in the bank  " << std::endl;
+		std::cout << "   6. Save bank data " << std::endl;
+		std::cout << "   7. Load saved bank data  " << std::endl;
+		std::cout << "   8. Overview mode (with deposit & withdraw functions)  " << std::endl;
+		std::cout << "   0. Exit  " << std::endl;
+		std::cout << "   ---------------------------------------------   " << std::endl;
+		std::cout << "\n   Wybierz numer polecenia :";
+
+		wybor = _getch();                                                     
+
+		if (wybor == 49)    //1
+		{
+			system("cls");
+			myBank->newAccount();
+		}
+		else if (wybor == 50)   //2
+		{
+			system("cls");
+			myBank->deleteAccount();
+		}
+		else if (wybor == 51)    //3
+		{
+			system("cls");
+			std::cout << "\n\n przelew\n\n";
+			system("pause");
+
+		}
+		else if (wybor == 52)   //4
+		{
+			system("cls");
+			myBank->displayAllAccounts();
+			system("pause");
+		}
+		else if (wybor == 53)   //5
+		{
+			if(myBank->howManyAccounts() == 1)
+				std::cout << "\n\n There is " << myBank->howManyAccounts() << " account in the bank\n\n";
+			else
+				std::cout << "\n\n There are "<< myBank->howManyAccounts() <<" accounts in the bank\n\n";
+			system("pause");
+		}
+		else if (wybor == 54)   //6
+		{
+			std::cout << "\n\n zapisywanie banku\n\n";
+			system("pause");
+		}
+		else if (wybor == 55)   //7
+		{
+			std::cout << "\n\n wczytywanie banku z pliku .txt\n\n";
+			system("pause");
+		}
+		else if (wybor == 56)   //8
+		{
+			system("cls");
+			std::cout << "\n\n overwiew mode \n\n";
+			myBank->overviewMode();
+			system("pause");
+		}
+	} while (wybor != 48); //0
+	
+
+	delete myBank;
+
+	system("pause");
+	return 0;
+}
